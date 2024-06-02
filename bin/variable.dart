3b7ed9297f946e13ab4c8de0776c6e36ae335453
Sandydht <sandydwi75@gmail.com>
2024-06-02 12:46:53 +0700

    feat: conversion

--- bin/variable.dart
+++ bin/variable.dart
@@ -1,7 +1,24 @@
 void main() {
-  String myName = 'Sandy Dwi Handoko Trapsilo';
-  var age = 30;
+  String fullName = 'Sandy Dwi Handoko Trapsilo';
+  var firstName = 'Sandy';
 
-  print(myName);
-  print(age);
+  print('full name: ' + fullName);
+  print('first name: ' + firstName);
+
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  array1[0] = 10;
+
+  print(array1);
+  print(array2);
+
+  late var value = getValue();
+  print('Variable sudah dibuat');
+  print(value);
+}
+
+String getValue() {
+  print('getValue() dipanggil');
+  return 'Sandy Dwi Handoko Trapsilo';
 }
