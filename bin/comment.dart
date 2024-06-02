d8ab51065fb35414c2f9b889144f3b290c32e591
Sandydht <sandydwi75@gmail.com>
2024-06-02 11:37:58 +0700

    feat: comment

---
+++ bin/comment.dart
@@ -0,0 +1,13 @@
+/// this is main function
+/// will executed by dart
+
+void main() {
+  // this is name variable
+  var name = 'Sandy Dwi Handoko Trapsilo';
+
+  /*
+   * this is multi line comment
+   * you can add more here
+   */
+  print(name);
+}
