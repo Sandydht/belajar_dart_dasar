6fb11500a8ea7c520304330438a8708868a5ad5e
Sandydht <sandydwi75@gmail.com>
2024-06-02 19:45:02 +0700

    feat: ternary operator

---
+++ bin/ternary_operator.dart
@@ -0,0 +1,15 @@
+void main() {
+  var nilai = 75;
+  var ucapan = nilai >= 75 ? 'Selamat Anda lulus' : 'Silahkan coba lagi';
+  print(ucapan);
+  
+  // String ucapan;
+
+  // if (nilai >= 75) {
+  //   ucapan = 'Selamat Anda lulus';
+  // } else {
+  //   ucapan = 'Silahkan coba lagi';
+  // }
+
+  // print(ucapan);
+}
