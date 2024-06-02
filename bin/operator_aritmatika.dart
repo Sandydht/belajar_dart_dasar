ffea0b659632c493d809f44d7351e28f28a40474
Sandydht <sandydwi75@gmail.com>
2024-06-02 12:54:26 +0700

    feat: operator

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,10 @@
+void main() {
+  var first = 10;
+  var second = 7;
+
+  print(first + second);
+  print(first - second);
+  print(first / second);
+  print(first ~/ second);
+  print(first % second);
+}
