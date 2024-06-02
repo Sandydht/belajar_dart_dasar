eb81db36f6106888fb74d445e25991afbb4b99f1
Sandydht <sandydwi75@gmail.com>
2024-06-02 13:04:50 +0700

    feat: operator logika

---
+++ bin/operator_logika.dart
@@ -0,0 +1,9 @@
+void main() {
+  var nilai1 = true;
+  var nilai2 = false;
+
+  print(nilai1 && nilai2);
+  print(nilai1 || nilai2);
+  print(!nilai1);
+  print(!nilai2);
+}
---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,17 @@
+void main() {
+  var a = 8;
+
+  print(a += 10);
+  print(a -= 10);
+  print(a *= 10);
+  // print(a /= 4);
+  print(a ~/= 10);
+  print(a %= 10);
+
+  var i = 0;
+  // i++; // i = i + 1
+  var j = i++;
+
+  print(i);
+  print(j);
+}
